c5eae9e191e92cd60801aec22e6f92a92e960c6c
Vitaliia Stepashkina <107074158+imunellka@users.noreply.github.com>
2025-02-01 18:28:05 +0100

    Create FraudDetectionModel_vers2.py

---
+++ models/FraudDetectionModel_vers2.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from transformers import Trainer, TrainingArguments
+from sklearn.metrics import roc_auc_score
+from transformers import DataCollatorForLanguageModeling, DataCollatorWithPadding
+
+
+class FraudDetectionModel(nn.Module):
+    def __init__(self, bert_model, hidden_size, num_classes=2):
+        super(FraudDetectionModel, self).__init__()
+        self.bert = bert_model
+        self.classifier = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_size // 2, num_classes)
+        )
+
+        # freezed BERT
+        for param in self.bert.bert.parameters():
+            param.requires_grad = False
+
+        for param in self.bert.bert.encoder.layer[-1].parameters():
+            param.requires_grad = True
+
+    def forward(self, input_ids, attention_mask=None, labels=None):
+        # Obtaining hidden states from BERT
+        outputs = self.bert.bert(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        hidden_states = outputs.hidden_states
+        last_hidden_state = hidden_states[-1]
+        pooled_output = last_hidden_state[:, 0, :]  # Using [CLS] token
+        logits = self.classifier(pooled_output)
+
+        if labels is not None:
+            loss_fn = nn.CrossEntropyLoss()
+            loss = loss_fn(logits, labels)
+            return loss, logits
+        return logits
